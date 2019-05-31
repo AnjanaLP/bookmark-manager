e83b4706e777b53809e3fecec4ef7a446f7f5762
Anjana <anjana.lpn@gmail.com>
2019-05-31 13:08:18 +0100

    setup test environment

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,6 +4,11 @@ describe Bookmark do
 
   describe '.all' do
     it 'returns all the bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://bbc.co.uk');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://cnn.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://google.co.uk');")
+
       bookmarks = Bookmark.all
       expect(Bookmark.all).to include "http://bbc.co.uk"
       expect(Bookmark.all).to include "http://cnn.com"
